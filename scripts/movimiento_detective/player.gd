716edfc75a2d047c775cebe61b2eeea9b62c0840
Santiago Solarte <171728510+ssolarte22@users.noreply.github.com>
2026-01-31 12:50:43 -0500

    Debugging fixes

--- scripts/movimiento_detective/player.gd
+++ scripts/movimiento_detective/player.gd
@@ -1,60 +1,46 @@
 extends CharacterBody2D
 
-@onready var _animated_sprite = $AnimatedSprite2D
-	
-const speed = 60 # Increased for visible movement
-var current_dir = null
+const speed = 60 
+var current_dir = "down" # Default direction
 
-func _physics_process(delta):
-	player_movement(delta)
-	move_and_slide() # This is required to actually move!
+func _physics_process(_delta): # Underscore tells Godot we aren't using delta here
+	player_movement() # Removed delta
+	move_and_slide()  # Keep this one
 	
-func player_movement(delta):
+func player_movement(): # Removed delta
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
+		velocity = Vector2(speed, 0)
 		play_anim(1)
-		velocity.x = speed
-		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
 		current_dir = "left"
+		velocity = Vector2(-speed, 0)
 		play_anim(1)
-		velocity.x = -speed
-		velocity.y = 0
 	elif Input.is_action_pressed("ui_up"):
 		current_dir = "up"
+		velocity = Vector2(0, -speed)
 		play_anim(1)
-		velocity.x = 0
-		velocity.y = -speed # Changed to Y axis
 	elif Input.is_action_pressed("ui_down"):
 		current_dir = "down"
+		velocity = Vector2(0, speed)
 		play_anim(1)
-		velocity.x = 0
-		velocity.y = speed # Changed to Y axis
 	else:
+		velocity = Vector2.ZERO
 		play_anim(0)
-		velocity.x = 0
-		velocity.y = 0
-	move_and_slide()
+	# move_and_slide() was removed from here
 
 func play_anim(movement):
-	var dir = current_dir
 	var anim = $AnimatedSprite2D
 	
-	if dir == "right":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("Move_right")
-	if dir == "left":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("Move_left")
-	if dir == "down":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("Move_down")
-	if dir == "up":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("Move_up")
-		
-		
+	if movement == 0:
+		anim.stop() # Or play an "idle" animation
+		return
+
+	if current_dir == "right":
+		anim.play("Move_right")
+	elif current_dir == "left":
+		anim.play("Move_left")
+	elif current_dir == "down":
+		anim.play("Move_down")
+	elif current_dir == "up":
+		anim.play("Move_up")
